cc2183f3760dc80ed047993b8e7e848013a95c9c
Naresh Karthigeyan <nareshkarthigeyan.2005@gmail.com>
2025-06-25 00:05:48 +0530

    cleanup

--- Player/Script/player.gd
+++ Player/Script/player.gd
@@ -2,7 +2,6 @@ class_name Player extends CharacterBody2D
 
 const WALK_SPEED = 100.0
 const RUN_SPEED = 350.0
-#const JUMP_VELOCITY = -300.0
 const DOUBLE_TAP_TIME = 0.3  # in seconds
 
 @onready var AnimatedPlayerSprite : AnimatedSprite2D = $AnimatedSprite2D
@@ -18,13 +17,10 @@ var run_direction = 0
 func _ready() -> void:
 	state_machine.Initialize(self)
 
-# TEMP
 func _unhandled_input(event):
 	state_machine._unhandled_input(event)
-	#if event is InputEventScreenTouch:
-		#if event.pressed:
-			#handle_touch(event.position)
 
+## FOR TOUCHSCREEN:
 func handle_touch(pos: Vector2):
 	var screen_width = get_viewport_rect().size.x
 	var screen_height = get_viewport_rect().size.y
@@ -51,69 +47,8 @@ func _input(event):
 		Input.action_release("ui_left")
 		Input.action_release("ui_right")
 		Input.action_release("ui_accept")
-		
-# END OF TEMP
+
 
 func _physics_process(delta: float) -> void:
-	#var current_time = Time.get_ticks_msec() / 1000.0  # Convert to seconds
-#
-	## Apply gravity
-	##if not is_on_floor():
-		##velocity.y += ProjectSettings.get_setting("physics/2d/default_gravity") * delta
-##
-	### Jump
-	##if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		##velocity.y = JUMP_VELOCITY
-#
-	## Direction input
-	#var direction = Input.get_axis("ui_left", "ui_right")
-	#print("Direction ",  direction)
-	#var input_pressed = ""
-#
-	#if Input.is_action_just_pressed("ui_left"):
-		#input_pressed = "ui_left"
-	#elif Input.is_action_just_pressed("ui_right"):
-		#input_pressed = "ui_right"
-#
-	## Handle double-tap for running
-	#if input_pressed != "":
-		#if current_time - last_input_time[input_pressed] <= DOUBLE_TAP_TIME:
-			#if input_pressed == "ui_right":
-				#run_direction = 1
-			#else:
-				#run_direction = -1
-		#else:
-			#run_direction = 0
-#
-		#last_input_time[input_pressed] = current_time
-	#
-	## TEMPORARY BULLSHIT:
-	#
-#
-	## Movement and animation
-	#if direction != 0:
-		#var speed = WALK_SPEED
-		#if direction == run_direction and run_direction != 0:
-			#speed = RUN_SPEED
-#
-		#velocity.x = direction * speed
-#
-		## Flip sprite
-		#if direction > 0:
-			#sprite.scale.x = 1
-		#else:
-			#sprite.scale.x = -1
-#
-		## Animation
-		#if speed == RUN_SPEED:
-			#if AnimatedPlayerSprite.animation != "run":
-				#AnimatedPlayerSprite.play("run")
-		#else:
-			#if AnimatedPlayerSprite.animation != "walk":
-				#AnimatedPlayerSprite.play("walk")
-	#else:
-		#velocity.x = move_toward(velocity.x, 0, WALK_SPEED)
-		#if AnimatedPlayerSprite.animation != "idle":
-			#AnimatedPlayerSprite.play("idle")
 	state_machine._physics_process(delta)
 	move_and_slide()
